1cf8615f6aeb3f00180a96f029d4f47932ec8c8e
ajiththomas <ajith>
2022-12-07 19:36:41 -0800

    react todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
 
+
+import './sss.css'
+import{useState} from 'react'
 function App() {
+  
+const[state,setCount]=useState('')
+const[states,setCount2]=useState([])
+        
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="app">
+    <div className="mainHeading">
+      <h1>ToDo List</h1>
     </div>
+    <div className="subHeading">
+      <br />
+      <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+    </div>
+    <div className="input">
+      <input value={state} onChange={(e)=>setCount(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+      <i onClick={()=>setCount2([...states,{id:Date.now(),text: state,status:false}])} className="fas fa-plus"></i>
+    </div>
+    
+    
+    <div className="todos">
+    {states.map((obj)=>{
+      return (
+      <div className="todo">
+        <div className="left">
+          <input onChange={(e)=>{
+            console.log(e.target.checked);
+            console.log(obj);
+            setCount2(states.filter(obj2=>{
+              if(obj2.id==obj.id){
+                obj2.status=e.target.checked
+              }
+              return obj2
+            }))
+          }} value={obj.status} type="checkbox" name="" id="" />
+          <p>{obj.text}</p>
+
+        </div>
+        <div className="right">
+          <i className="fas fa-times"></i>
+        </div>
+      </div>);
+      })
+    }
+    <h1>active takes</h1>
+    {states.map((obj)=>{
+      if(obj.status){
+        return(<h2>{obj.text}</h2>);
+    }
+      return null
+  })}
+    </div>
+   </div>
+
+  
+      
+       
   );
 }
 
-export default App;
+export default App
